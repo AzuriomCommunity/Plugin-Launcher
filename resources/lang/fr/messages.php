4597918578d4d34ef827e572175e7bce4db81d28
Oximov <Oximov@users.noreply.github.com>
2020-08-18 21:06:15 +0200

    correct gramatical error

--- resources/lang/fr/messages.php
+++ resources/lang/fr/messages.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title' => 'Télécharger nôtre Launcher',
+    'title' => 'Télécharger notre Launcher',
     'desc1' => 'Ce launcher te permettra de venir jouer sur le serveur avec nous et de rejoindre les ',
     'desc2' => ' joueurs connecté !',
     'serveroff' => 'Ce launcher te permettra de venir jouer sur le serveur avec nous et de rejoindre les ERREUR joueurs connecté !',
